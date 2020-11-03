e031ddd673ba8c2f167737b2357c5dbb179a60cc
longjiyan <1751102099@qq.com>
2020-11-03 21:15:14 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/longdd/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.longdd.git;
+
+/**
+ * @author longjiyan
+ * @create 2020-11-03 20:38
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+
+}
